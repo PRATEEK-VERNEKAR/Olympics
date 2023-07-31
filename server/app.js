5d7366242d763d87f598a11e25791250ea000ed3
Prateek Vernekar <mprateekvernekar189@gmail.com>
2023-07-31 23:13:34 +0530

    done

--- server/app.js
+++ server/app.js
@@ -64,6 +64,11 @@ app.get("/", (req, res) => {
     res.send(req.cookies)
  });
 
+ 
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>Page not found on the server</h1>")
+})
 
 app.listen(PORT,()=>{
     console.log("Server running on ",PORT);
